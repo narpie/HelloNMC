e366f39198b78f1e35063c4820802b0200b40af1
Bayley Haddix <haddixb@ad.nmc.edu>
2019-10-30 10:34:33 -0400

    Add project files.

---
+++ HelloNMC/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloNMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
